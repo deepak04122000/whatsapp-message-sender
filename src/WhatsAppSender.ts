4a7116f0ac379bd98be04252e75ae1317549688c
Adarsh <sanakihacker@gmail.com>
2021-07-15 14:07:38 +0530

    error handled

--- src/WhatsAppSender.ts
+++ src/WhatsAppSender.ts
@@ -25,7 +25,13 @@ async function scrap(urls: string[]) {
   const browser = await puppeteer.launch({ headless: false });
   let page = await browser.newPage();
   for (let index = 0; index < urls.length; index++) {
-    await sendProcess(page, urls[index], index);
+    try {
+      await sendProcess(page, urls[index], index);
+    } catch (e) {
+      console.log(urls[index]);
+    } finally {
+      continue;
+    }
   }
   console.log("Done");
   browser.close();
@@ -39,19 +45,14 @@ const sendProcess = async (
   await page.goto(url);
   await page.waitForFunction("window.alert");
   await page.keyboard.press("Enter");
-  await page.waitForSelector('span[title="P P Group"]', { timeout: 100000 });
-  // const target = await page.$('span[title="MB"]');
-  // await target.click();
-  // const input = await page.$(
-  //   "#main > footer > div.vR1LG._3wXwX.copyable-area > div._2A8P4 > div > div._2_1wd.copyable-text.selectable-text"
-  // );
-  // await input.type("HEllo from code.....");
-  if (index === 0) {
-    console.log(index);
-    await wait(6000);
-  } else {
-    await wait(1000);
-  }
+  const phone = url.split("&")[0].split("+91")[1];
+  const p1 = phone.slice(0, 5);
+  const p2 = phone.slice(5, 10);
+
+  await page.waitForSelector(`span[title="+91 ${p1} ${p2}"]`, {
+    timeout: 15000,
+  });
+  await wait(3000);
   await page.keyboard.press("Enter");
   await wait(4000);
 };
